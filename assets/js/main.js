9ffeb95377882d573087918ba301e3f8001561a3
Jake <jake0oo0andminecraft@gmail.com>
2016-08-16 14:34:46 +0000

    more work on displaying params

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
 $(function() {
 
-	// var $nav = $('.sidebar');
-
-	$('.sidebar').scrollspy({
-		// min: $nav.offset().top,
-		// onEnter: function(element, position) {
-		// 	$nav.addClass('fixed');
-		// },
-		// onLeave: function(element, position) {
-		// 	$nav.removeClass('fixed');
-		// }
-	});
+	// $('#tabs a').click(function (e) {
+	//   e.preventDefault()
+	//   $(this).tab('show')
+	// })
 
 });
