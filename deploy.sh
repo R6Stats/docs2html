cbbd634dec436a43bcd98dfd0c4c854385eeb118
Jake0oo0 <jake0oo0dev@gmail.com>
2016-08-19 00:53:22 +0000

    Clean old files on push

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ rm -rf dev.r6stats.com/
 git config user.email "support@r6stats.com"
 git config user.name "R6Stats Deploybot"
 git clone git@github.com:R6Stats/dev.r6stats.com.git
+rm -r dev.r6stats.com/*
 cp -r build/* dev.r6stats.com/
 cd dev.r6stats.com/
 ls
